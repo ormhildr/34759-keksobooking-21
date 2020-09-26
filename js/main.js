094afea4fb617a68c9241d0c1e48e4871d1b2776
Dmitry <bigvolkovski@gmail.com>
2020-09-26 21:06:48 +0300

    Исправления в генерации значений

--- js/main.js
+++ js/main.js
@@ -79,28 +79,33 @@ const shuffle = (array) => {
   return doubleArr;
 };
 
-const generateAd = () => ({
-  author: {
-    avatar: `img/avatars/user0${getRandom(1, AMOUNT_ADS)}.png`
-  },
-  offer: {
-    title: TITLES[getRandom(0, TITLES.length - 1)],
-    address: `${location.x}, ${location.y}`,
-    price: getRandom(MIN_PRICE, MAX_PRICE),
-    type: TYPES[getRandom(0, TYPES.length - 1)],
-    rooms: getRandom(1, AMOUNT_ROOMS),
-    guests: getRandom(1, AMOUNT_GUESTS),
-    checkin: TIME[getRandom(0, TIME.length - 1)],
-    checkout: TIME[getRandom(0, TIME.length - 1)],
-    features: shuffle(ALL_FEATURES).slice(getRandom(0, ALL_FEATURES.length)),
-    description: DESCRIPTIONS[getRandom(0, DESCRIPTIONS.length - 1)],
-    photos: shuffle(ALL_PHOTOS).slice(getRandom(0, ALL_PHOTOS.length))
-  },
-  location: {
-    x: getRandom(0, MAP_WIDTH),
-    y: getRandom(MIN_Y, MAX_Y)
-  }
-});
+const generateAd = () => {
+  const LOCATION_X = getRandom(PIN_WIDTH, MAP_WIDTH);
+  const LOCATION_Y = getRandom(MIN_Y, MAX_Y);
+
+  return {
+    author: {
+      avatar: `img/avatars/user0${getRandom(1, AMOUNT_ADS)}.png`
+    },
+    offer: {
+      title: TITLES[getRandom(0, TITLES.length - 1)],
+      address: `${LOCATION_X}, ${LOCATION_Y}`,
+      price: getRandom(MIN_PRICE, MAX_PRICE),
+      type: TYPES[getRandom(0, TYPES.length - 1)],
+      rooms: getRandom(1, AMOUNT_ROOMS),
+      guests: getRandom(1, AMOUNT_GUESTS),
+      checkin: TIME[getRandom(0, TIME.length - 1)],
+      checkout: TIME[getRandom(0, TIME.length - 1)],
+      features: shuffle(ALL_FEATURES).slice(getRandom(0, ALL_FEATURES.length)),
+      description: DESCRIPTIONS[getRandom(0, DESCRIPTIONS.length - 1)],
+      photos: shuffle(ALL_PHOTOS).slice(getRandom(0, ALL_PHOTOS.length))
+    },
+    location: {
+      x: LOCATION_X,
+      y: LOCATION_Y
+    }
+  };
+};
 
 const getAds = () => {
   const ads = [];
@@ -112,10 +117,11 @@ const getAds = () => {
 
 const renderAd = (ad) => {
   const adElement = pinTemplate.cloneNode(true);
+  const imgAd = adElement.querySelector(`img`);
 
-  adElement.querySelector(`.map__pin`).style = `left: ${ad.location.x + PIN_WIDTH}px; top: ${ad.location.y + PIN_HEIGHT}px;`;
-  adElement.querySelector(`img`).src = ad.author.avatar;
-  adElement.querySelector(`img`).alt = ad.offer.title;
+  adElement.style = `left: ${ad.location.x - PIN_WIDTH}px; top: ${ad.location.y - PIN_HEIGHT}px;`;
+  imgAd.src = ad.author.avatar;
+  imgAd.alt = ad.offer.title;
 
   return adElement;
 };
