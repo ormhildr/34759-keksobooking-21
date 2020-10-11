e7db707068e935608707f67448cdb32743692fe4
Dmitry <bigvolkovski@gmail.com>
2020-10-11 13:18:53 +0300

    Добавлена работа с post запросом

--- js/form.js
+++ js/form.js
@@ -8,9 +8,63 @@
     100: `«не для гостей»`
   };
 
-
+  const adForm = document.querySelector(`.ad-form`);
   const roomsAmount = document.querySelector(`#room_number`);
   const guestsAmount = document.querySelector(`#capacity`);
+
+  const errorTemplate = document.querySelector(`#error`).content.querySelector(`.error`);
+  const errorElement = errorTemplate.cloneNode(true);
+  const errorBtn = errorElement.querySelector(`.error__button`);
+
+  const successTemplate = document.querySelector(`#success`).content.querySelector(`.success`);
+  const successElement = successTemplate.cloneNode(true);
+
+  const btnReset = adForm.querySelector(`.ad-form__reset`);
+
+  const errorWindow = () => {
+    document.body.insertAdjacentElement(`afterbegin`, errorElement);
+
+    errorBtn.addEventListener(`click`, () => {
+      errorElement.remove();
+    });
+
+    document.addEventListener(`keydown`, (evt)=> {
+      if (evt.key === `Escape`) {
+        evt.preventDefault();
+        errorElement.remove();
+      }
+    });
+
+    errorElement.addEventListener(`click`, ()=> {
+      errorElement.remove();
+    });
+  };
+
+  const successWindow = () => {
+    document.body.insertAdjacentElement(`afterbegin`, successElement);
+
+    document.addEventListener(`keydown`, (evt)=> {
+      if (evt.key === `Escape`) {
+        evt.preventDefault();
+        successElement.remove();
+      }
+    });
+
+    successElement.addEventListener(`click`, ()=> {
+      successElement.remove();
+    });
+  };
+
+  const resetForm = () => {
+    document.querySelectorAll(`.map__pin`).forEach((el) => {
+      if (!el.classList.contains(`map__pin--main`)) {
+        el.remove();
+      }
+    });
+    document.querySelector(`.map__card`).remove();
+    adForm.reset();
+    window.map.disablePage();
+  };
 
   const getGuestsLimits = (rooms) => {
     return rooms <= 3 ? [1, rooms] : [0];
@@ -33,7 +87,20 @@
   roomsAmount.addEventListener(`change`, validateGuests);
   guestsAmount.addEventListener(`change`, validateGuests);
 
+  adForm.addEventListener(`submit`, (evt) => {
+    window.backend.save(new FormData(adForm), () => {
+      successWindow();
+      resetForm();
+    }, errorWindow);
+    evt.preventDefault();
+  });
+
+  btnReset.addEventListener(`click`, () => {
+    resetForm();
+  });
+
   window.form = {
+    adForm,
     validateGuests
   };
 })();
