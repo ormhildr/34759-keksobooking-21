d0eecd73d0c424e4e969fac56cf6b50e30de995e
Dmitry <bigvolkovski@gmail.com>
2020-10-13 16:32:32 +0300

    Добавлена работа с карточками объявлений и валидация формы

--- js/pin.js
+++ js/pin.js
@@ -5,26 +5,17 @@
 
   const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
 
-  const renderPin = (ad) => {
-    const adElement = pinTemplate.cloneNode(true);
-    const imgAd = adElement.querySelector(`img`);
-
-    adElement.style = `left: ${ad.location.x - MAIN_PIN_SIZE}px; top: ${ad.location.y - MAIN_PIN_SIZE}px;`;
-    imgAd.src = ad.author.avatar;
-    imgAd.alt = ad.offer.title;
-
-    return adElement;
-  };
-
   window.pin = {
     MAIN_PIN_SIZE,
-    renderPins: (arr) => {
-      const fragment = document.createDocumentFragment();
+    renderPin: (ad) => {
+      const adElement = pinTemplate.cloneNode(true);
+      const imgAd = adElement.querySelector(`img`);
 
-      for (let i = 0; i < arr.length; i++) {
-        fragment.appendChild(renderPin(arr[i]));
-      }
-      return fragment;
+      adElement.style = `left: ${ad.location.x - MAIN_PIN_SIZE}px; top: ${ad.location.y - MAIN_PIN_SIZE}px;`;
+      imgAd.src = ad.author.avatar;
+      imgAd.alt = ad.offer.title;
+
+      return adElement;
     }
   };
 })();
